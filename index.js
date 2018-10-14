893f6dac387d74acc5ae0ad20edb66b7cbdf1696
Jody Neckles <jody@technative.tv>
2018-10-14 14:46:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string == string.toLowerCase()) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
-}
+
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
